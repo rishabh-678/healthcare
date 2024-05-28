9bc190330e17cec935eb1cce2d270e7fd48a5a73
rishabh-678 <146519629+rishabh-678@users.noreply.github.com>
2024-05-28 15:57:49 +0530

    Add files via upload

---
+++ assets/js/function.js
@@ -0,0 +1,75 @@
+$('#home_banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+$('#health_package').owlCarousel({
+    loop:false,
+    margin:20,
+    nav:false,
+    dots:true,
+    
+    responsive:{
+        0:{
+            items:1.3                                                             
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('#client_testimonial').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+$('#Explore_dprt').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dots:true,
+
+    responsive:{
+        0:{
+            items:1.3
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
+
+
+
